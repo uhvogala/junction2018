dad8632e46621bfda7f4f15943e840a13a829175
vertti <vertti.aito@aalto.fi>
2018-11-24 22:13:14 +0200

    added state to index.js

--- fleetapi/FleetApp/index.js
+++ fleetapi/FleetApp/index.js
@@ -6,29 +6,50 @@ import Gauges from "./Gauges"
 
 
 class HelloWorld extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { apiData: {} };
+  } 
+
+  handleNewData = (event) => {
+    let data = {};
+    data[event.name] = event.returnValue;
+    const { apiData } = this.state;
+
+    this.setState({apiData: { ...apiData, ...data }});
+  }
+
+  componentWillMount = () => {
+    //addEventListener();
+  }
+
+  componentWillUnmount = () => {
+    //removeEventListener();
+  }
+
   render() {
     return (
-<View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{width: '60%', backgroundColor: "#14162c"}}>
-            <Gauges />
-          </View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: '60%', backgroundColor: "#14162c"}}>
+          <Gauges />
+        </View>
 
-          <View style={{flex: 1, flexDirection: "column", width: "40%", backgroundColor: "#14162c"}}>
-            <Leaderboard />
-            <DriverBonuses />
-          </View>
+        <View style={{flex: 1, flexDirection: "column", width: "40%", backgroundColor: "#14162c"}}>
+          <Leaderboard data={this.state.apiData}/>
+          <DriverBonuses data={this.state.apiData}/>
+        </View>
 
-          {/*<View style={{width: '50%', backgroundColor: 'skyblue'}}>
-            <View style={{height: '50%', backgroundColor: 'red'}}>
-              <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
-              <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
-            </View>
-            <View style={{height: '50%', backgroundColor: 'green'}}>
-              <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
-              <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
-            </View>
-          </View>*/}
-</View>
+        {/*<View style={{width: '50%', backgroundColor: 'skyblue'}}>
+          <View style={{height: '50%', backgroundColor: 'red'}}>
+            <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
+            <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
+          </View>
+          <View style={{height: '50%', backgroundColor: 'green'}}>
+            <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
+            <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
+          </View>
+        </View>*/}
+      </View>
     );
   }
 }
