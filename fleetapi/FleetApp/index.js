099af6c10d1b9c1c775bdf9b0106624f7214bc4a
Mikael <mikael@pop-os.localdomain>
2018-11-24 17:23:56 +0200

    clean up

--- fleetapi/FleetApp/index.js
+++ fleetapi/FleetApp/index.js
@@ -5,7 +5,7 @@ class HelloWorld extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.hello}>Suck my balls</Text>
+        <Text style={styles.hello}>This is a test</Text>
       </View>
     );
   }
