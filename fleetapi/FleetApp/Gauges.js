e4d4efbdb2ee7ea2ad57fb87b1523e2bf5c9e5af
kastanja <johml@student.uef.fi>
2018-11-24 20:32:03 +0200

    yee

---
+++ fleetapi/FleetApp/Gauges.js
@@ -0,0 +1,176 @@
+import React, {Component} from "react"
+import {StyleSheet, Text, View} from 'react-native';
+import Speedometer from 'react-native-speedometer-chart';
+
+var totalValue = 100;
+
+class Gauges extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      drivingScore: 0,
+      totalRange: 5000,
+      fuelLevel: 1500,
+      fuelConsumption: 40,
+      drivingScoreColor: '#e60808'
+    }
+  }
+
+  componentWillMount() {
+    setInterval(() => this.setDrivingScore(), 1);
+    setInterval(() => this.setFuelConsumption(), 100);
+    setInterval(() => this.setTotalRange(), 300);
+
+  }
+
+  setDrivingScore(){
+   
+    this.setState({
+      drivingScore: this.state.totalRange - this.state.fuelConsumption 
+    })
+
+
+    // if(this.state.drivingScore >= 0){
+    //   this.setState({
+    //     drivingScore: this.state.drivingScore + 1 //Math.random() * 50
+    //   })
+    // }
+    // else{
+    //   this.setState({
+    //     drivingScore: 0 //Math.random() * 50
+    //   })
+    // }
+
+    // set colors
+
+    //red
+    this.setState({
+      drivingScoreColor: '#e60808'
+    })
+
+    // orange
+    if(this.state.drivingScore > 25){
+      this.setState({
+        drivingScoreColor: '#ca7d22'
+      })
+    }
+
+    // yellow
+    if(this.state.drivingScore > 50){
+      this.setState({
+        drivingScoreColor: '#cac522'
+      })
+    }
+
+    // green
+    if(this.state.drivingScore > 75){
+      this.setState({
+        drivingScoreColor: '#14a02e'
+      })
+    }
+  }
+
+  setFuelConsumption(){
+    if(this.state.fuelConsumption < 100){
+      this.setState({
+        fuelConsumption: this.state.fuelConsumption - Math.floor(Math.random()*(4-(-4)+1)+(-4))
+      })
+    }
+    else if(this.state.fuelConsumption < 0){
+      this.setState({
+        fuelConsumption: 50
+      })
+    }
+    else{
+      this.setState({
+        fuelConsumption: 50 //Math.random() * 50
+      })
+    }
+
+    // set colors
+
+    // //red
+    // this.setState({
+    //   drivingScoreColor: '#e60808'
+    // })
+
+    // // orange
+    // if(this.state.drivingScore > 25){
+    //   this.setState({
+    //     drivingScoreColor: '#ca7d22'
+    //   })
+    // }
+
+    // // yellow
+    // if(this.state.drivingScore > 50){
+    //   this.setState({
+    //     drivingScoreColor: '#cac522'
+    //   })
+    // }
+
+    // // green
+    // if(this.state.drivingScore > 75){
+    //   this.setState({
+    //     drivingScoreColor: '#14a02e'
+    //   })
+    // }
+  }
+
+  setTotalRange(){
+    this.setState({
+      totalRange: this.state.fuelLevel / (this.state.fuelConsumption / 100)
+    })
+  }
+
+
+
+
+  render() {
+    return (
+      <View elevation={5} style={styles.cont}>
+        <Text style={styles.title}>Driving score</Text>
+        <Speedometer value={this.state.drivingScore} totalValue={7000} style={styles.gauge} 
+          internalColor={this.state.drivingScoreColor} showLabels={true} />
+
+        <Text style={styles.title}>Fuel consumption</Text>
+        <Speedometer value={this.state.fuelConsumption} totalValue={80} style={styles.gauge} showLabels={true}/>
+
+        <Text style={styles.title}>Total range</Text>
+        <Speedometer value={this.state.totalRange} totalValue={8000} style={styles.gauge} showLabels={true}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  cont: {
+    height: "50%"
+  },
+  title: {
+    fontSize: 35,
+    textAlign: "center",
+    fontWeight: "200",
+    marginTop: 30
+  },
+
+  leaderboardCont: {
+    marginTop: 20,
+    backgroundColor: "white",
+    margin: 10,
+    marginBottom: 0,
+    borderRadius: 4
+  },
+
+  leaderboardDriver: {
+    textAlign: "center",
+    fontSize: 25,
+    paddingVertical: 5,
+    borderBottomWidth: 1
+  },
+  gauge: {
+    marginLeft: '35%'
+  }
+})
+
+export default Gauges
--- fleetapi/FleetApp/index.js
+++ fleetapi/FleetApp/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import Leaderboard from './Leaderboard';
+import DriverBonuses from "./DriverBonuses"
+import Gauges from "./Gauges"
+
 
 class HelloWorld extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>This is a test</Text>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{width: '60%', backgroundColor: 'powderblue'}}>
+        <Text style={styles.welcome}>INSERT TITLE HERE</Text>
+        <Text style={styles.instructions}>KPI GOES IN HERE</Text>
+        <Gauges />
       </View>
+
+      <View style={{flex: 1, flexDirection: "column", width: "40%", backgroundColor: "#eee"}}>
+        <Leaderboard />
+        <DriverBonuses />
+      </View>
+
+      {/*<View style={{width: '50%', backgroundColor: 'skyblue'}}>
+        <View style={{height: '50%', backgroundColor: 'red'}}>
+          <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
+        </View>
+        <View style={{height: '50%', backgroundColor: 'green'}}>
+          <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
+        </View>
+      </View>*/}
+    </View>
     );
   }
 }
