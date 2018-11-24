e216889f3a2cd5200008485203cadd2368113103
kastanja <johml@student.uef.fi>
2018-11-24 13:58:38 +0200

    Test for developing UI

---
+++ junctionTest/App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{width: '50%', backgroundColor: 'powderblue'}}>
+            <Text style={styles.welcome}>INSERT TITLE HERE</Text>
+            <Text style={styles.instructions}>KPI GOES IN HERE</Text>
+
+          </View>
+          <View style={{width: '50%', backgroundColor: 'skyblue'}}>
+            <View style={{height: '50%', backgroundColor: 'red'}}>
+              <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
+              <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
+            </View>
+            <View style={{height: '50%', backgroundColor: 'green'}}>
+              <Text style={styles.instructions}>To g1234567et staradsdsadsated, edit App.js</Text>
+              <Text style={styles.instructions}>{instructions}dsdssddrrrs</Text>
+            </View>
+          </View>
+        </View>
+
+
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
